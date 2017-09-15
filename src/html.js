3faaace3f3b6831b8df763c4c627d7f7c017d16d
fabien0102 <fabien0102@planee.fr>
2017-09-15 18:59:42 +0200

    Add meta

--- src/html.js
+++ src/html.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+const meta = {
+  title: "@fabien0102 website",
+  description: "Développeur fullstack JavaScript, voici un tour d’horizon de mes différents projets.",
+  image: "https://fabien0102.com/card.jpg",
+  url: "https://fabien0102.com",
+  author: "@fabien0102"
+};
+
 let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
@@ -48,7 +56,20 @@ export default class HTML extends React.Component {
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3ea191" />
           <meta name="theme-color" content="#ffffff" />
-          <title>fabien0102</title>
+          <title>@fabien0102</title>
+          <meta name="description" content={meta.description} />
+          {/* Twitter meta */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content={meta.author} />
+          <meta name="twitter:creator" content={meta.author} />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+          {/* Facebook meta */}
+          <meta property="og:url" content={meta.url} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:image" content={meta.image} />
           {css}
         </head>
         <body>
