aff73394f60e21290303461120289829b2598caa
fabien0102 <fabien0102@planee.fr>
2017-09-15 19:00:49 +0200

    Add manifest and offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,18 @@ module.exports = {
       }
     },
     "gatsby-transformer-yaml",
-    "gatsby-transformer-remark"
+    "gatsby-transformer-remark",
+     {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Fabien0102 website",
+        short_name: "Fabien0102",
+        start_url: "/",
+        background_color: "#012f56",
+        theme_color: "#3ea191",
+        display: "minimal-ui",
+      },
+    },
+    "gatsby-plugin-offline"
   ]
 }
