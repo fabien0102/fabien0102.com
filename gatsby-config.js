99fb25d0bf5b6f7f307e3fd1a5d919e052e57354
fabien0102 <fabien0102@planee.fr>
2017-10-03 16:47:23 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     "gatsby-transformer-yaml",
     "gatsby-transformer-remark",
-     {
+    {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: "Fabien0102 website",
@@ -18,9 +18,15 @@ module.exports = {
         start_url: "/",
         background_color: "#012f56",
         theme_color: "#3ea191",
-        display: "minimal-ui",
-      },
+        display: "minimal-ui"
+      }
     },
-    "gatsby-plugin-offline"
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-107444198-1"
+      }
+    }
   ]
-}
+};
