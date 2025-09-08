8f591ce494f21b0b9dfa250bbe5046fe688e010d
Fabien Bernard <fabien0102@hotmail.com>
2025-09-08 17:02:29 +0200

    Add netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sentry(), spotlightjs()],
+  adapter: netlify(),
 });
