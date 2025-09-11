e9ba2964feb8df9db7c978670dd4b13643dbd4ea
Fabien Bernard <fabien0102@hotmail.com>
2025-09-11 10:15:11 +0200

    Polish responsive design

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 const curriculum = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "data/curriculum" }),
